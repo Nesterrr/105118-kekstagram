5e486db5e6f465993cf9a84290b2c40ebf18bc64
Nesterrr <2626246q@gmail.com>
2016-10-07 13:20:54 +0300

    Поправил недочёты

--- src/js/check.js
+++ src/js/check.js
@@ -1,30 +1,33 @@
 ﻿'use strict'
 
-var message;
-
 function getMessage(a,b) {
-  if (typeof(a) == "boolean") {
+  var message;
+  var lastCheck = [];
+  
+  if (typeof a === "boolean") {
     if (a == true) {
       message = "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+      lastCheck[0] = message;
     }
     else {
       message = "Переданное GIF-изображение не анимировано";
+      lastCheck[1] = message;
     }
   }
 
-  if (typeof(a) == "number") {
+  if (typeof a === "number") {
     message = "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
+    lastCheck[2] = message;
   }
 
-  if (Array.isArray(a)) {
-    var i = 0;
+  if (Array.isArray(a) && !Array.isArray(b)) {	
     var amountOfRedPoints = 0;
 
-    while (i < a.length) {
+    for(var i = 0; i < a.length; i++) {
       amountOfRedPoints += a[i];
-      i++;
     }
     message = "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+    lastCheck[3] = message;
   }
 
   if (Array.isArray(a) && Array.isArray(b)) {
@@ -34,9 +37,12 @@ function getMessage(a,b) {
       artifactsSquare += a[i] * b[i];
     }
     message = "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+    lastCheck[4] = message;
   }
 
+  if(message !== lastCheck[0] && message !== lastCheck[1] && message !== lastCheck[2] && message !== lastCheck[3] && message !== lastCheck[4]) {
+  	message = "Переданы некорректные данные";
+  }
+  
   return message;
 }
-
-getMessage();
