fb7c4778623740d84ae0a6ccc6a177d76969f8c3
Nesterrr <2626246q@gmail.com>
2016-10-10 17:43:48 +0300

    Добавил полупрозрачную рамку и текст

--- src/js/resizer.js
+++ src/js/resizer.js
@@ -133,20 +133,20 @@
         (this._container.height));
 
       this._ctx.rect(
-        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth ,
-        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth ,
-        this._resizeConstraint.side + this._ctx.lineWidth/2 ,
-        this._resizeConstraint.side + this._ctx.lineWidth/2 );
-      this._ctx.fillStyle = "rgba(0,0,0,0.8)";
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
+        this._resizeConstraint.side + this._ctx.lineWidth / 2,
+        this._resizeConstraint.side + this._ctx.lineWidth / 2 );
+      this._ctx.fillStyle = 'rgba(0,0,0,0.8)';
       this._ctx.fill('evenodd');
       this._ctx.restore();
 
-      this._ctx.font = "20px Open Sans";
-      this._ctx.fillStyle = "#ffffff";
-      this._ctx.fillText(this._image.naturalWidth + ' x ' + this._image.naturalHeight,this._image.naturalWidth/2 - 40,58);
+      this._ctx.font = '20px Open Sans';
+      this._ctx.fillStyle = '#ffffff';
+      this._ctx.fillText(this._image.naturalWidth + ' x ' + this._image.naturalHeight, this._image.naturalWidth / 2 - 40, 58);
 
-      this._ctx.font = "20px Open Sans";
-      this._ctx.fillStyle = "#ffffff";
+      this._ctx.font = '20px Open Sans';
+      this._ctx.fillStyle = '#ffffff';
     },
 
     /**
