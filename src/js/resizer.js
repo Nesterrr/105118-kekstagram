195a9ce09fa352a3dfa9dbcbb12a28dfb4bd97b8
Nesterrr <2626246q@gmail.com>
2016-10-11 13:40:50 +0300

    Изменил способ отрисовки фона

--- src/js/resizer.js
+++ src/js/resizer.js
@@ -126,19 +126,26 @@
       // некорректно сработает даже очистка холста или нужно будет использовать
       // сложные рассчеты для координат прямоугольника, который нужно очистить.
 
-      this._ctx.rect(
-        ((-this._container.width / 2)),
-        ((-this._container.height / 2)),
-        (this._container.width),
-        (this._container.height));
+      this._ctx.beginPath();
+      this._ctx.strokeStyle = 'rgba(0,0,0,0.0)';
+      this._ctx.moveTo((-this._container.width / 2), (-this._container.height / 2));
+      this._ctx.lineTo((-this._container.width / 2), (this._container.height / 2));
+      this._ctx.lineTo((this._container.width / 2), (this._container.height / 2));
+      this._ctx.lineTo((this._container.width / 2), (-this._container.width / 2));
+      this._ctx.closePath();
 
-      this._ctx.rect(
-        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
-        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
-        this._resizeConstraint.side + this._ctx.lineWidth / 2,
-        this._resizeConstraint.side + this._ctx.lineWidth / 2 );
+      this._ctx.moveTo((-this._container.width / 2) + 70, (-this._container.height / 2) + 70);
+      this._ctx.lineTo((-this._container.width / 2) + 70, (this._container.height / 2) - 78);
+      this._ctx.lineTo((this._container.width / 2) - 78, (this._container.height / 2) - 78);
+      this._ctx.lineTo((this._container.width / 2) - 78, (-this._container.width / 2) + 70);
+
+      this._ctx.closePath();
+
       this._ctx.fillStyle = 'rgba(0,0,0,0.8)';
       this._ctx.fill('evenodd');
+
+      this._ctx.stroke();
+
       this._ctx.restore();
 
       this._ctx.font = '20px Open Sans';
