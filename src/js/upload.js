140d01ebd5ee2a4b2b1ae5c6618f3cc6a7eebf12
Nesterrr <2626246q@gmail.com>
2016-10-17 17:32:38 +0300

    ошибка

--- src/js/upload.js
+++ src/js/upload.js
@@ -261,6 +261,7 @@
     filterImage.className = 'filter-image-preview ' + filterMap[selectedFilter];
   };
 
+
   var resizeX = document.querySelector('#resize-x');
   var resizeY = document.querySelector('#resize-y');
   var resizeSize = document.querySelector('#resize-size');
