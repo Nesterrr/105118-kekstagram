5124672688b6b5833b50c4a0626ebee759e741d2
nesterrr <2626246q@gmail.com>
2016-10-30 20:35:32 +0300

    Область видимости

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,12 +1,66 @@
 'use strict';
 
-define([
-  'load',
-  'review'
-], function(load, review) {
-  load(PIC_URL, function(pictures) {
+define(['./review.js'], function(review){
+  var cbName = 'cb';
+  var PIC_URL = 'http://localhost:1507/api/pictures';
+
+  var load = function(url, callback) {
+    window[cbName] = function(data) {
+      callback(data);
+    };
+
+    var script = document.createElement('script');
+    script.src = url + '?callback=' + cbName;
+    document.body.appendChild(script);
+  };
+  var IMG_SIZE = 182;
+
+  // Находим .filters и прячем его
+  var filters = document.querySelector('.filters');
+  filters.classList.add('hidden');
+
+  // Блок внутри которого будут добавлены картинки
+  var picturesContainer = document.querySelector('.pictures');
+
+  // Шаблон
+  var template = document.querySelector('#picture-template');
+  var templateContainer = 'content' in template ? template.content : template;
+
+  var picElement = templateContainer.querySelector('.picture');
+
+  // Функция напонения шаблона данными
+
+  //***************************************
+  /*
+  var addDataInTemplate = function(pic) {
+    var nodeClone = picElement.cloneNode(true);
+
+
+    nodeClone.querySelector('.picture-comments').textContent = pic.comments;
+
+    nodeClone.querySelector('.picture-likes').textContent = pic.likes;
+    var img = new Image();
+
+    img.onload = function(evt) {
+      var nodeImg = nodeClone.querySelector('img');
+      nodeImg.src = evt.target.src;
+      nodeImg.width = IMG_SIZE;
+      nodeImg.heigth = IMG_SIZE;
+    };
+    img.onerror = function() {
+      nodeClone.classList.add('picture-load-failure');
+    };
+    img.src = pic.url;
+    return nodeClone;
+  };
+*/
+  // Функуция отрисовки шаблонов
+  var renderPictures = function(pictures) {
     pictures.forEach(function(pic) {
       picturesContainer.appendChild(addDataInTemplate(pic));
+
     });
-  });
+  };
+
+  load(PIC_URL, renderPictures);
 });
