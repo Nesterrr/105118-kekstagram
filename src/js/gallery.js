f19dc0129758d6c79720d05505a9d1d6b5e3edc6
Nesterrr <2626246q@gmail.com>
2016-11-08 16:46:16 +0300

    Вторая компонента

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['./load'], function() {
+define(['./load', './review'], function() {
   function Gallery(pictures, activePictureNum) {
     this.pictures = pictures;
     this.activePicture = activePictureNum;
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['./review.js', './load.js', './gallery'], function(addDataInTemplate, load, gallery) {
+define(['./review.js', './load.js', './gallery'], function( picture, load, gallery) {
   var PIC_URL = 'http://localhost:1507/api/pictures';
 
   var picturesContainer = document.querySelector('.pictures');
@@ -8,7 +8,9 @@ define(['./review.js', './load.js', './gallery'], function(addDataInTemplate, lo
   var renderPictures = function(pictures) {
     gallery.setPictures(pictures);
     pictures.forEach(function(pic, i) {
-      picturesContainer.appendChild(addDataInTemplate(pic, i));
+      picture.setData(pic);
+      picturesContainer.appendChild(picture.addDataInTemplate());
+      picture.addEvntHandl(i);
     });
   };
   load(PIC_URL, renderPictures);
