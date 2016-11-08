57d188eef788a48e67d5b54b264d40d93c928530
Svyatoslav Nesteruk <2626246q@gmail.com>
2016-11-08 11:51:30 +0300

    Объект «Галерея» (#16)
    * Галерея_начало
    
    * Доделать дома
    
    * не работает
    
    * Добавил галлерею
    
    * Поправил недочёты
    
    * Передал

---
+++ src/js/gallery.js
@@ -0,0 +1,51 @@
+'use strict';
+
+define(['./load'], function() {
+  function Gallery(pictures, activePictureNum) {
+    this.pictures = pictures;
+    this.activePicture = activePictureNum;
+
+    this.galleryOverlay = document.querySelector('.gallery-overlay');
+    this.galleryOverlayClose = document.querySelector('.gallery-overlay-close');
+    this.galleryOverlayImage = document.querySelector('.gallery-overlay-image');
+  }
+
+  Gallery.prototype.setPictures = function(pictures) {
+    this.pictures = pictures;
+  };
+
+  Gallery.prototype.show = function(num) {
+    var self = this;
+
+    this.galleryOverlay.classList.remove('invisible');
+    this.setActivePicture(num);
+
+    this.galleryOverlayClose.onclick = function() {
+      self.hide();
+    };
+
+    this.galleryOverlayImage.onclick = function() {
+      num++;
+      if(num === self.pictures.length) {
+        num = 0;
+      }
+      self.setActivePicture(num);
+    };
+  };
+
+  Gallery.prototype.hide = function() {
+    this.galleryOverlay.classList.add('invisible');
+    this.galleryOverlayImage.onclick = null;
+    this.galleryOverlayClose.onclick = null;
+  };
+
+  Gallery.prototype.setActivePicture = function(num) {
+    this.activePicture = num;
+    var self = this;
+
+    self.galleryOverlayImage.setAttribute('src', this.pictures[num].url);
+    document.querySelector('.likes-count').innerHTML = this.pictures[num].likes;
+    document.querySelector('.comments-count').innerHTML = this.pictures[num].comments;
+  };
+  return new Gallery();
+});
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,13 +1,14 @@
 'use strict';
 
-define(['./review.js', './load.js'], function(addDataInTemplate, load) {
+define(['./review.js', './load.js', './gallery'], function(addDataInTemplate, load, gallery) {
   var PIC_URL = 'http://localhost:1507/api/pictures';
 
   var picturesContainer = document.querySelector('.pictures');
 
   var renderPictures = function(pictures) {
-    pictures.forEach(function(pic) {
-      picturesContainer.appendChild(addDataInTemplate(pic));
+    gallery.setPictures(pictures);
+    pictures.forEach(function(pic, i) {
+      picturesContainer.appendChild(addDataInTemplate(pic, i));
     });
   };
   load(PIC_URL, renderPictures);
