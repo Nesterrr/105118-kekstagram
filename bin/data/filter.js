f2ce4e1814e2ce43c7ce68d10bdf70fc261cd3b4
Nesterrr <2626246q@gmail.com>
2016-11-11 17:18:20 +0300

    Поправил сортировку

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -2,51 +2,24 @@
 
 module.exports = function(list, filterID) {
   var result = list;
-
-  var bubbleSort = function(picsArray, filterID) {
-    var n = picsArray.length;
-    for (var i = 0; i < n-1; i++)
-    {
-      for (var j = 0; j < n-1-i; j++)
-      {
-        if(filterID === 'filter-popular') {
-          if (picsArray[j + 1].likes > picsArray[j].likes) {
-            var t = picsArray[j + 1];
-            picsArray[j + 1] = picsArray[j];
-            picsArray[j] = t;
-          }
-        }
-        if(filterID === 'filter-discussed') {
-          if (picsArray[j + 1].comments > picsArray[j].comments) {
-            var t = picsArray[j + 1];
-            picsArray[j + 1] = picsArray[j];
-            picsArray[j] = t;
-          }
-        }
-        if(filterID === 'filter-new') {
-          if (picsArray[j + 1].created > picsArray[j].created) {
-            var t = picsArray[j + 1];
-            picsArray[j + 1] = picsArray[j];
-            picsArray[j] = t;
-          }
-        }
-
-      }
-    }
-    return picsArray;
-  }
-
   switch(filterID) {
     case 'filter-popular': {
-      result = bubbleSort(list, filterID);
+      result.sort(function(a, b){
+        return b.likes - a.likes;
+      });
       break;
     }
     case 'filter-new': {
-     result = bubbleSort(list, filterID);
+      result.sort(function(a, b){
+        return b.created - a.created;
+      });
+
       break;
     }
     case 'filter-discussed':
-      result = bubbleSort(list, filterID);
+      result.sort(function(a, b){
+      return b.comments - a.comments;
+      });
       break;
   }
   return result;
